a6f6646ead14c506636f33547fcdb48221d8f9da
shydesky <shydesky@gmail.com>
2016-11-06 23:59:07 +0800

    user

--- model/modelx.py
+++ model/modelx.py
@@ -3,12 +3,12 @@ from database import Base, db_session
 
 class User(Base):
     __tablename__ = 'users'
-    id = Column(Integer, primary_key=True)
-    name = Column(String(50), unique=True)
+    id = Column(Integer, primary_key=True) 
     email = Column(String(120), unique=True)
-    passwd = Column(String(120), unique=True)
-    phone = Column(String(11), unique=True)
-    clientKey = Column(String(120), unique=True)
+    name = Column(String(50))
+    passwd = Column(String(120))
+    phone = Column(String(11))
+    clientKey = Column(String(120))
 
     def __init__(self, name=None, email=None, passwd=None, phone=None, clientKey=None):
         self.name = name
--- operation/op_service.py
+++ operation/op_service.py
@@ -7,13 +7,14 @@ def op_get_user(id):
 def op_signup(**kwargs):
     email = kwargs.get('email','')
     phone = kwargs.get('phone','')
-    passwd= kwargs.get('passwd','')
+    passwd = kwargs.get('passwd','')
+    name = kwargs.get('name','')
     users = User.query.filter_by(email=email).all()
     import pdb;pdb.set_trace()
     if users:
         return 'user exist'
 
-    user = User(email=email,phone=phone,passwd=passwd,clientKey='')
+    user = User(name=name,email=email,phone=phone,passwd=passwd,clientKey='')
     db_session.add(user)
     db_session.commit()
     
