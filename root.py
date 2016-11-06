ab14aefed0c8f5ea3e85252a723b0f7e790b944e
shydesky <shydesky@gmail.com>
2016-11-06 22:13:13 +0800

    ll

--- root.py
+++ root.py
@@ -2,10 +2,14 @@ from flask import Flask
 from controller import service
 app = Flask(__name__)
 
-
 @app.route("/")
 def hello():
     return "Hello World!"
+
+@app.route("/service", methods=['POST', 'GET'])
+def service():
+	userid = request.form['userid']
+    service.get_user(userid)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
