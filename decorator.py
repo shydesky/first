452d5217bdc0d372435835aae00aecaf72b3540b
shy <sunhaoyu@papayamobile.cn>
2016-11-08 16:27:46 +0800

    s

---
+++ decorator.py
@@ -0,0 +1,18 @@
+from model import *
+
+from functools import wraps
+
+def permission_check(func):
+    @wraps(func)
+    def new_func(self, *args, **kwargs):
+        ret = {}
+        key = kwargs.get('token','')
+        email = kwargs.get('email','')
+        user = User.query.filter(and_(User.clientKey==key,User.email==email)).first()
+        if user:
+            return func(self, *args, **kwargs)
+        else:
+            ret['msg'] = 'Permission deny'
+            ret['data'] = {}
+            return ret
+    return new_func
