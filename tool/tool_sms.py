1a2ff95ae232f5182e4271dc27d0766665d1ccd3
shy <sunhaoyu@papayamobile.cn>
2016-11-14 17:56:17 +0800

    tool sms

---
+++ tool/tool_sms.py
@@ -0,0 +1,13 @@
+import requests;
+import json;
+def send_messag_example():
+    resp = requests.post(("http://api.weimi.cc/2/sms/send.html"),
+    data={
+        "uid": "AvwM2OSpi1VD",
+        "pas": "u3zhavh4",
+        "mob": "15901009909",
+        "con": "【微米】您的验证码是：610912，3分钟内有效。如非您本人操作，可忽略本消息。",
+        "type": "json"
+    },timeout=3 , verify=False);
+    result =  json.loads( resp.content )
+    print result
