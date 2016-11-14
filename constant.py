6e9dcdafacf0d3701b5c73680f8df7f3626b31bd
shydesky <shydesky@gmail.com>
2016-11-14 22:05:40 +0800

    cc

---
+++ constant.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python2.7
+# -*- coding: utf-8 -*-
+SUCCESS='成功'
+SIGNUP_SUCCESS='注册成功'
+USER_EXIST='此用户已存在'
+USER_NOT_EXIST='此用户不存在'
+PASSWORD_RESET_SUCCESS='密码重置成功'
+VERIFYCODE_IS_INVALID='验证码错误'
+VERIFYCODE_IS_SEND='验证码已发送'
+DEPOSIT_SUCCESS='充值成功'
+CALC_SUCCESS='计算成功'
+ADMIN_USER_NOT_EXIST='此管理员用户不存在'
+ADMIN_USER_LOGIN='管理员用户登陆成功'
