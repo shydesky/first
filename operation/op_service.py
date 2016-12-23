9fa75a6eca98a5036157c36c16fab4698f502231
shydesky <shydesky@gmail.com>
2016-12-23 21:49:19 +0800

    zero

--- operation/op_service.py
+++ operation/op_service.py
@@ -181,29 +181,34 @@ def process_calc(kwargs, index):
 def op_calc(arg1, arg2, index):
     ret = {}
     data = {}
-    if index == '1':
-        data['X1'] = "%.5f" % round((arg2/arg1*2.0-1)*arg1, 5)
-        data['X2'] = "%.5f" % round((arg2/arg1*3.0-2)*arg1, 5)
-        data['X3'] = "%.5f" % round((arg2/arg1*4.0-3)*arg1, 5)
-        data['X4'] = "%.5f" % round((arg2/arg1*5.0-4)*arg1, 5)
-        data['X5'] = "%.5f" % round((arg2/arg1*6.0-5)*arg1, 5)
-        data['X6'] = "%.5f" % round((arg2/arg1*7.0-6)*arg1, 5)
-        data['X7'] = "%.5f" % round((arg2/arg1*8.0-7)*arg1, 5)
-        data['X8'] = "%.5f" % round((arg2/arg1*9.0-8)*arg1, 5)
-    elif index == '2':
-        data['Y1'] = "%.5f" % round(arg2/(arg2/arg1*2.0-1), 5)
-        data['Y2'] = "%.5f" % round(arg2/(arg2/arg1*3.0-2), 5)
-        data['Y3'] = "%.5f" % round(arg2/(arg2/arg1*4.0-3), 5)
-        data['Y4'] = "%.5f" % round(arg2/(arg2/arg1*5.0-4), 5)
-        data['Y5'] = "%.5f" % round(arg2/(arg2/arg1*6.0-5), 5)
-        data['Y6'] = "%.5f" % round(arg2/(arg2/arg1*7.0-6), 5)
-        data['Y7'] = "%.5f" % round(arg2/(arg2/arg1*8.0-7), 5)
-        data['Y8'] = "%.5f" % round(arg2/(arg2/arg1*9.0-8), 5)
+    try:
+        if index == '1':
+            data['X1'] = "%.5f" % round((arg2/arg1*2.0-1)*arg1, 5)
+            data['X2'] = "%.5f" % round((arg2/arg1*3.0-2)*arg1, 5)
+            data['X3'] = "%.5f" % round((arg2/arg1*4.0-3)*arg1, 5)
+            data['X4'] = "%.5f" % round((arg2/arg1*5.0-4)*arg1, 5)
+            data['X5'] = "%.5f" % round((arg2/arg1*6.0-5)*arg1, 5)
+            data['X6'] = "%.5f" % round((arg2/arg1*7.0-6)*arg1, 5)
+            data['X7'] = "%.5f" % round((arg2/arg1*8.0-7)*arg1, 5)
+            data['X8'] = "%.5f" % round((arg2/arg1*9.0-8)*arg1, 5)
+        elif index == '2':
+            data['Y1'] = "%.5f" % round(arg2/(arg2/arg1*2.0-1), 5)
+            data['Y2'] = "%.5f" % round(arg2/(arg2/arg1*3.0-2), 5)
+            data['Y3'] = "%.5f" % round(arg2/(arg2/arg1*4.0-3), 5)
+            data['Y4'] = "%.5f" % round(arg2/(arg2/arg1*5.0-4), 5)
+            data['Y5'] = "%.5f" % round(arg2/(arg2/arg1*6.0-5), 5)
+            data['Y6'] = "%.5f" % round(arg2/(arg2/arg1*7.0-6), 5)
+            data['Y7'] = "%.5f" % round(arg2/(arg2/arg1*8.0-7), 5)
+            data['Y8'] = "%.5f" % round(arg2/(arg2/arg1*9.0-8), 5)
+    except ZeroDivisionError:
+        ret['data'] = {}
+        ret['msg'] = ZERODIVISION
+        return ret
 
     ret['data'] = data
     ret['msg'] = CALC_SUCCESS
-    return ret
-
+    return ret    
+    
 @permission_check_admin
 def op_get_all_user():
     ret = {}
