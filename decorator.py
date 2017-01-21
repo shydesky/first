e5e8f00f47fb894234720183bc352ce5426870c2
shy <sunhaoyu@papayamobile.cn>
2017-01-21 18:13:30 +0800

    ooo

--- decorator.py
+++ decorator.py
@@ -3,13 +3,14 @@ from flask import request
 from functools import wraps
 from constant import *
 import datetime
-
+import hashlib
 def permission_check(func):
     @wraps(func)
     def new_func(*args, **kwargs):
         ret = {}
-        key = request.args.get('token','')
-        phone = request.args.get('account','')
+        key = request.args.get('token', '')
+        phone = request.args.get('account', '')
+        key = hashlib.md5(phone + key).hexdigest()
         user = User.query.filter(and_(User.clientKey == key, User.phone == phone)).first()
         if not user:
             ret['msg'] = 'Permission deny'
