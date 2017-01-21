3ac947b107ba320954c544545a89a8e7d105b590
shy <sunhaoyu@papayamobile.cn>
2017-01-21 16:00:15 +0800

    code

--- tool/tool_sms.py
+++ tool/tool_sms.py
@@ -2,15 +2,15 @@
 import requests;
 import json;
 def send_message_example(code,phone):
-    print code,phone
+    print code, phone
     resp = requests.post(("http://api.weimi.cc/2/sms/send.html"),
     data={
-        "uid": "AvwM2OSpi1VD",
-        "pas": "u3zhavh4",
+        "uid": "f2Ns6qnlML1c",
+        "pas": "nf4wxhx9",
         "mob": phone,
         "con": '【微米】您的验证码是：%s，10分钟内有效。如非您本人操作，可忽略本消息。' % code,
         "type": "json"
-    },timeout=3 , verify=False);
-    result =  json.loads( resp.content )
+    },timeout=3, verify=False);
+    result = json.loads(resp.content)
     print result
     return result
