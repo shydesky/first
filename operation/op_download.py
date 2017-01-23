661ce4b3b2eca0361c8d21896439b697a716ab40
shy <sunhaoyu@papayamobile.cn>
2017-01-23 12:59:53 +0800

    exe

--- operation/op_download.py
+++ operation/op_download.py
@@ -1,4 +1,5 @@
 from flask import make_response
+from flask import send_file
 def op_download_app():
     response = make_response(send_file("fengbaoyan.exe"))
     response.headers["Content-Disposition"] = "attachment; filename=fengbaoyan.exe;"
