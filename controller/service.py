3f11074b6a992e0bcb8a26988088a229caeb9f2d
shydesky <shydesky@gmail.com>
2017-01-31 13:15:55 +0800

    ss

--- controller/service.py
+++ controller/service.py
@@ -30,7 +30,7 @@ def process_user():
     return ret
 
 def process_admin():
-    resp = make_response(url_for('index'))
+    resp = make_response('<a href="%s">index</a>' % url_for('index'))
     username = request.form['username']
     password = request.form['password']
     ret = op_service.op_admin_login(username, password)
