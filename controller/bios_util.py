608cd7793741b58fa3598f8e9d07eced6fc50c85
shy <sunhaoyu@papayamobile.cn>
2017-02-05 13:23:42 +0800

    disk

--- controller/bios_util.py
+++ controller/bios_util.py
@@ -1,8 +1,8 @@
 import wmi
 
 def get_disk_info():
-
     encrypt_str = ""
+    c = wmi.WMI()
     for physical_disk in c.Win32_DiskDrive():
         encrypt_str += physical_disk.SerialNumber.strip()
         return encrypt_str
